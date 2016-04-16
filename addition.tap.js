c6be5a9df610345aa6d6b52f04758495b2bbd07e
wly731 <wly731@gmail.com>
2016-04-16 12:02:32 -0700

    add test

---
+++ addition.tap.js
@@ -0,0 +1,14 @@
+var tap = require('tap')
+
+var addition = require('./addition') //.js is implied
+
+tap.test('should add two numbers', function(t){
+  var first = 7
+  var second = 12
+
+  var combined = addition(first, second)
+  t.equal(combined, 19)
+  t.end()
+}) 
+
+
